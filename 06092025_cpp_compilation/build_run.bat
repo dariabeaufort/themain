01f6d2e006d02b4bf07104b8a719c808ce55f3d5
dariabeaufort <zdavydova10@gmail.com>
2025-09-24 17:03:44 +0300

    a

--- 06092025_cpp_compilation/build_run.bat
+++ 06092025_cpp_compilation/build_run.bat
@@ -2,10 +2,13 @@
 chcp 1251 > log
 del log
 
-set MAIN=main
-set EXE=example
+set MAIN=main.cpp
+set EXE=example.exe
+
 set CHARSET="-finput-charset=utf-8 -fexec-charset=windows-1251"
 
-g++ "%CHARSET%" %MAIN%.cpp -o %EXE%
+if exist %EXE% del %EXE%
 
-example.exe
+g++ "%CHARSET%" %MAIN% -o %EXE%
+
+%EXE%
--- 06092025_cpp_compilation/main.cpp
+++ 06092025_cpp_compilation/main.cpp
@@ -2,9 +2,8 @@
 #include <string>
 
 int main() {
-	int k = 5;
 	std::cout << "Введите приветствие: ";
-	std:: string str;
+	std::string str;
 	std::getline(std::cin, str);
 	std::cout << str << std::endl;
 }
